161f9e635f4898d4d08aff911f6dcbf65a932d86
thao.ho <hphthao1705@gmail.com>
2024-07-12 16:39:30 +0700

    Update key

--- app/src/main/java/com/example/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/example/weatherapp/utils/Constants.kt
@@ -3,9 +3,8 @@ package com.example.weatherapp.utils
 object Constants {
     const val BASE_URL = "https://api.weatherapi.com/"
     const val END_POINT = "v1/current.json?"
-    const val KEY = "3ab6dcb8cf05430fa7065535232208"
+    const val KEY = "e0f033957e784ed999e34806242406"
 
     const val BASE_URL_CITY = "https://countriesnow.space/"
     const val END_POINT_CITY = "api/v0.1/countries/population/cities"
-    const val END_POINT_ERROR = "aaaaa"
 }
