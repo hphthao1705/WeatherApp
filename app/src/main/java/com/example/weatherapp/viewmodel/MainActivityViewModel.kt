4e566c9b4f80959c5ef9d7e0064bb0c0de67d8e2
thao.ho <hphthao1705@gmail.com>
2024-07-30 18:06:59 +0700

    Add new API load country with flags (2)

--- app/src/main/java/com/example/weatherapp/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/MainActivityViewModel.kt
@@ -4,38 +4,35 @@ import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.weatherapp.data.model.Data
-import com.example.weatherapp.repository.CityRepository
+import com.example.weatherapp.data.usecase.CityUseCase
 import com.example.weatherapp.utils.AppUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class MainActivityViewModel(private val cityRepository: CityRepository) : ViewModel() {
+class MainActivityViewModel(private val cityUseCase: CityUseCase) : ViewModel() {
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
     val doneAPI: MutableLiveData<Boolean> = MutableLiveData()
-    var list: ArrayList<Data>? = arrayListOf()
     var errorVisibility: MutableLiveData<Int> = MutableLiveData()
 
     init {
-        loadCities()
+//        loadCities()
+        loadCities2()
     }
 
     private fun loadCities() {
         try {
             viewModelScope.launch(Dispatchers.IO) {
-                cityRepository.loadCity().collect {
+                cityUseCase.getCityUIViewModel().collect {
                     it.onRight {
                         withContext(Dispatchers.Main) {
-                            it?.data?.let { it1 ->
-                                val response = async { AppUtils.saveListCity(it1) }
-                                list?.addAll(it1)
-                                response.await()?.let {
-                                    doneAPI.postValue(true)
-                                }
+                            val response = async { AppUtils.saveListCity(it) }
+                            response.await()?.let {
+                                doneAPI.postValue(true)
                             }
                         }
+                        errorVisibility.postValue(View.GONE)
                     }
                     it.onLeft {
                         errorVisibility.postValue(View.VISIBLE)
@@ -45,7 +42,30 @@ class MainActivityViewModel(private val cityRepository: CityRepository) : ViewMo
             }
         } catch (ex: Exception) {
             errorVisibility.postValue(View.VISIBLE)
-        } finally {
+        }
+    }
+
+    private fun loadCities2() {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                cityUseCase.getCityUIViewModel2().collect {
+                    it.onRight {
+                        withContext(Dispatchers.Main) {
+                            val response = async { AppUtils.saveListCity(it) }
+                            response.await()?.let {
+                                doneAPI.postValue(true)
+                            }
+                        }
+                        errorVisibility.postValue(View.GONE)
+                    }
+                    it.onLeft {
+                        errorVisibility.postValue(View.VISIBLE)
+                    }
+                }
+                loadingVisibility.postValue(View.GONE)
+            }
+        } catch (ex: Exception) {
+            errorVisibility.postValue(View.VISIBLE)
         }
     }
 }
